d8843e6a716f80cb2dc1c7fc4b10602fc2c67ea0
Lucía Tello <luciatellocolado@gmail.com>
2026-02-23 19:16:48 +0100

    Set Render backend URL for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 // API_URL will be replaced at build time with the real Render URL
 export const environment = {
   production: true,
-  apiUrl: 'RENDER_API_URL_PLACEHOLDER',  // We'll replace this after creating the Render service
+  apiUrl: 'https://herbia-server.onrender.com/api',
 };
