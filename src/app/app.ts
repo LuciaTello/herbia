c69cd62a7f13b57791f60fb9b5168b6c583d002c
Lucía Tello <luciatellocolado@gmail.com>
2026-02-25 22:14:43 +0100

    Make herbia logo clickable, link to homepage

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, effect, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { I18nService } from './i18n';
 import { LangSelector } from './components/lang-selector/lang-selector';
 import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LangSelector],
+  imports: [RouterOutlet, RouterLink, LangSelector],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
